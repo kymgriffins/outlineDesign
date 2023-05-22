bf08b75b91ec5556282876dd03827f37f0a119da
kymgriffins <kimutaigriffins1960@gmail.com>
2023-05-22 10:32:32 +0300

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Providers from '~/components/atoms/Providers';
 import Header from '~/components/widgets/Header';
 import Announcement from '~/components/widgets/Announcement';
 import Footer2 from '~/components/widgets/Footer2';
-
+import { Analytics } from '@vercel/analytics/react';
 import { Inter as CustomFont } from 'next/font/google';
 import '~/assets/styles/base.css';
 
@@ -33,6 +33,7 @@ export default function RootLayout({ children }: LayoutProps) {
       </head>
       <body className="bg-white tracking-tight text-gray-900 antialiased dark:bg-slate-900 dark:text-slate-300">
         <Providers>
+        <Analytics />
           {/* <Announcement /> */}
           <Header />
           <main>{children}</main>
