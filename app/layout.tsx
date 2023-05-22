390330603dc15a6cf8efe4dd87833dc0a52a31e5
kymgriffins <kimutaigriffins1960@gmail.com>
2023-05-22 17:30:13 +0300

    google assistent

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import Footer2 from '~/components/widgets/Footer2';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter as CustomFont } from 'next/font/google';
 import '~/assets/styles/base.css';
-
+import ReactGA from 'react-ga';
 const customFont = CustomFont({ subsets: ['latin'], variable: '--font-custom' });
 
 export interface LayoutProps {
@@ -23,14 +23,20 @@ export const metadata: Metadata = {
   },
   description: SITE.description,
 };
+const TRACKING_ID = "G-KNZS361QQP"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en" className={`motion-safe:scroll-smooth 2xl:text-[24px] ${customFont.variable} font-sans`}>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
+<meta charSet="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="description" content="Outline Designs is a premier construction company offering exceptional building design and construction services. Transforming architectural visions into reality with industry best practices, sustainability, and innovation." />
+<meta name="keywords" content="Outline Designs, construction company, building design, construction services, sustainability, innovation" />
+<meta name="author" content="Outline Designs" />
+<title>Outline Designs - Premier Construction Company</title>
+</head>
       <body className="bg-white tracking-tight text-gray-900 antialiased dark:bg-slate-900 dark:text-slate-300">
         <Providers>
         <Analytics />
