5c1d7131261c5cc670fc1c0a479cd315fe99a4c9
kymgriffins <kimutaigriffins1960@gmail.com>
2023-07-09 17:17:42 +0300

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,14 +32,26 @@ export default function RootLayout({ children }: LayoutProps) {
   }, []);
   return (
     <html lang="en" className={`motion-safe:scroll-smooth 2xl:text-[24px] ${customFont.variable} font-sans`}>
-      <head>
-<meta charSet="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<meta name="description" content="Outline Designs is a premier construction company offering exceptional building design and construction services. Transforming architectural visions into reality with industry best practices, sustainability, and innovation." />
-<meta name="keywords" content="Outline Designs, construction company, building design, construction services, sustainability, innovation" />
-<meta name="author" content="Outline Designs" />
-<title>Outline Designs - Premier Construction Company</title>
+<head>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Outline Designs is a premier construction company offering exceptional building design and construction services. Transforming architectural visions into reality with industry best practices, sustainability, and innovation." />
+    <meta name="keywords" content="Outline Designs, construction company, building design, construction services, sustainability, jenga, kenya, architectural firm , innovation" />
+    <meta name="author" content="Outline Designs" />
+    <meta name="robots" content="index, follow" />
+    <meta name="revisit-after" content="7 days" />
+    <meta name="language" content="English" />
+    <meta name="og:title" content="Outline Designs - Premier Construction Company" />
+    <meta name="og:description" content="Outline Designs is a  construction company offering exceptional building design and construction services. Transforming architectural visions into reality with industry best practices, sustainability, and innovation." />
+    <meta name="og:image" content="https://example.com/path-to-image.jpg" />
+    <meta name="og:url" content="https://example.com" />
+    <meta name="og:type" content="website" />
+    <meta name="twitter:title" content="Outline Designs - Premier Construction Company" />
+    <meta name="twitter:description" content="Outline Designs is a premier construction company offering exceptional building design and construction services. Transforming architectural visions into reality with industry best practices, sustainability, and innovation." />
+    <meta name="twitter:image" content="https://example.com/path-to-image.jpg" />
+    <title>Outline Designs - Premier Construction Company</title>
 </head>
+
       <body className="bg-white tracking-tight text-gray-900 antialiased dark:bg-slate-900 dark:text-slate-300">
         <Providers>
         <Analytics />
